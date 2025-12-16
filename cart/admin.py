13f13cb55a80699d2fb7c46b23126710fffcdd72
Arun Sabu <sabuarun00@gmail.com>
2025-12-16 21:09:07 +0530

    chore: register cart model in admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cart
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "product", "quantity", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "product__title")
