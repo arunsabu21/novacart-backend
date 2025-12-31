5c9640d1d5b0994789a59d97aa33288be18c42c4
Arun Sabu <sabuarun00@gmail.com>
2025-12-31 11:44:24 +0530

    updated signals

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,5 @@
 import resend
+from django.conf import settings
 from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
 
@@ -6,17 +7,21 @@ from django_rest_passwordreset.signals import reset_password_token_created
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, **kwargs):
 
-    reset_url = f"https://novacart-frontend-url/reset-password/{reset_password_token.user.pk}/{reset_password_token.key}"
+    # frontend reset page link
+    reset_url = f"http://localhost:5173/reset-password/{reset_password_token.user.pk}/{reset_password_token.key}"
+
+    # set API key
+    resend.api_key = settings.RESEND_API_KEY
 
     resend.Emails.send(
         {
             "from": "NovaCart <onboarding@resend.dev>",
-            "to": reset_password_token.email,
+            "to": reset_password_token.user.email,
             "subject": "Reset your NovaCart password",
             "html": f"""
-        <h2>Password Reset</h2>
-        <p>Click the link below to reset your password.</p>
-        <a href="{reset_url}">{reset_url}</a>
-        """,
+                <h2>Password Reset</h2>
+                <p>Click the link below to reset your password:</p>
+                <a href="{reset_url}">{reset_url}</a>
+            """,
         }
     )
