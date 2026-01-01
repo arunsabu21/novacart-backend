bee1f0782481ccdb8d9ec983d9573eab4f89b50e
Arun Sabu <sabuarun00@gmail.com>
2026-01-01 12:11:35 +0530

    fix: update ALLOWED_HOSTS for render

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ".onrender.com,localhost,127.0.0.1").split(",")
 
 # ---------- CORS & CSRF ----------
 
