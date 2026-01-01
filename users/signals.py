f9335f32422a143064daa13bd77e464d5baaf6e2
Arun Sabu <sabuarun00@gmail.com>
2026-01-01 13:33:54 +0530

    wip: temp local changes in signals.py

--- users/signals.py
+++ users/signals.py
@@ -10,7 +10,7 @@ from django.dispatch import receiver
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, **kwargs):
     # just log for now — NO EMAIL SENDING
-    reset_url = f"https://example.com/reset-password/{reset_password_token.user.pk}/{reset_password_token.key}"
+    reset_url = f"https://novacart-frontend.netlify.app/reset-password/{reset_password_token.user.pk}/{reset_password_token.key}"
     print("RESET URL:", reset_url)
     return
 
