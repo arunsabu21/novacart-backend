a911ebaae9ecbb3306a513add80412dc73e4c2cf
Arun Sabu <sabuarun00@gmail.com>
2026-01-01 20:00:51 +0530

    Temporary changes

--- users/views.py
+++ users/views.py
@@ -14,11 +14,11 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
-    
+
 
 class UserProfileView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request):
         serializer = ProfileSerializer(request.user)
         return Response(serializer.data)
@@ -26,14 +26,15 @@ class UserProfileView(APIView):
 
 class UpdateProfileView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def patch(self, request):
         serializer = ProfileSerializer(request.user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 @api_view(["POST"])
 def password_reset_confirm(request):
     token = request.data.get("token")
@@ -56,3 +57,14 @@ def password_reset_confirm(request):
 
     return Response({"detail": "Password Reset Successful"}, status=200)
 
+
+@api_view(["GET"])
+def create_temp_admin(request):
+    if User.objects.filter(username="tempadmin").exists():
+        return Response({"message": "Admin already exist"})
+
+    User.objects.create_superuser(
+        username="tempadmin", email="admin@example.com", password="Temp@12345"
+    )
+
+    return Response({"message": "Temporary admin created"})
