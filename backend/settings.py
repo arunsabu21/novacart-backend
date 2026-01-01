e8125664056a929bb2a80ed94a576974324be78b
Arun Sabu <sabuarun00@gmail.com>
2026-01-01 20:49:52 +0530

    switch database to postgres on render

--- backend/settings.py
+++ backend/settings.py
@@ -108,11 +108,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # ---------- DATABASE ----------
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # (Later  switch to Postgres)
