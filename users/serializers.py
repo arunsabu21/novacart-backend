c234e1f4a329249599c41f23ba9b5203dcb52e0a
Arun Sabu <sabuarun00@gmail.com>
2026-01-02 12:01:40 +0530

    Fix register serializer, Postgres 500 error

--- users/serializers.py
+++ users/serializers.py
@@ -9,21 +9,26 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ["username", "email", "password"]
 
-        def create(self, validate_data):
-            username = (validate_data["username"],)
-            email = (validate_data["email"],)
-            password = validate_data["password"]
+    def create(self, validated_data):
+        username = validated_data["username"]
+        email = validated_data.get("email")
+        password = validated_data["password"]
 
-            if User.objects.filter(username=username).exists():
-                raise serializers.ValidationError("Username already exist")
+        # username already exists
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError({"username": "Username already exists"})
 
-            if email and User.objects.filter(email=email).exists():
-                raise serializers.ValidationError("Email already exist")
+        # email already exists
+        if email and User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({"email": "Email already exists"})
 
-            user = User.objects.create_user(
-                username=username, email=email, password=password
-            )
-            return user
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        return user
 
 
 class ProfileSerializer(serializers.ModelSerializer):
