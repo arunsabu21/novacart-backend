420df4b6cb8f2ebe1a9815613372aa722c88c71f
Arun Sabu <sabuarun00@gmail.com>
2026-01-12 15:29:24 +0530

    ci: add backend GitHub Actions pipeline

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
 from .models import Cart
 
+
 class CartSerializer(serializers.ModelSerializer):
     product_title = serializers.CharField(source="product.title", read_only=True)
+    product_subtitle = serializers.SerializerMethodField()
     product_price = serializers.DecimalField(
-        source="product.price",
-        max_digits=10,
-        decimal_places=2,
-        read_only=True
+        source="product.price", max_digits=10, decimal_places=2, read_only=True
     )
     product_image = serializers.ImageField(source="product.image", read_only=True)
     total_price = serializers.SerializerMethodField()
@@ -18,11 +17,15 @@ class CartSerializer(serializers.ModelSerializer):
             "id",
             "product",
             "product_title",
+            "product_subtitle",
             "product_price",
             "product_image",
             "quantity",
             "total_price",
         ]
+
+    def get_product_subtitle(self, obj):
+        return obj.product.subtitle
 
     def get_total_price(self, obj):
         return obj.product.price * obj.quantity
