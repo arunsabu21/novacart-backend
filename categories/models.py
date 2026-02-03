966335ecc909b363752c130b44e7f01eac7f6446
Arun Sabu <sabuarun00@gmail.com>
2026-02-03 21:28:18 +0530

    feat: add categories and update product associations

--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,22 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to="categories/", blank=True, null=True)
+    image = models.ImageField(upload_to="categories/common/", blank=True, null=True)
+    mobile_image = models.ImageField(
+        upload_to="categories/mobile/",
+        blank=True,
+        null=True
+    )
+    mobile_secondary_image = models.ImageField(
+        upload_to="categories/mobile_secondary/",
+        blank=True,
+        null=True
+    )
+    desktop_image = models.ImageField(
+        upload_to="categories/desktop/",
+        blank=True,
+        null=True
+    )
     is_active = models.BooleanField(default=True)
     
     def __str__(self):
